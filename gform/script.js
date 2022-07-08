d59bc18985e6f6a464cc6dccd1f0532c62f7323c
rizan21 <rizanfarooqui@gmail.com>
2022-07-08 17:07:36 +0530

    Added script that shows inputs on console

---
+++ gform/script.js
@@ -0,0 +1,6 @@
+function showOnConsole(){
+    console.log(`Name         : "${document.getElementById('name').value}"`);
+    console.log(`College Name : "${document.getElementById('cname').value}"`);
+    console.log(`Email        : "${document.getElementById('email').value}"`);
+    console.log(`Mobile No    : "${document.getElementById('phno').value}"`);
+}
